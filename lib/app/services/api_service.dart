7bff029901048848ba3e3208388d34960d12e5f3
Urawit <urawit.j@ku.th>
2023-09-04 22:37:37 +0700

    initialize

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -4,7 +4,8 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiService extends GetConnect {
-  static const String apiBaseUrl = 'http://192.168.2.44/api'; // Laravel API URL
+  // static const String apiBaseUrl = 'http://192.168.2.44/api'; // Laravel API URL
+  static const String apiBaseUrl = 'http://192.168.1.114/api'; // Laravel API URL [TAM]
 
   Future<Map<String, dynamic>> postData(
       Map<String, dynamic> data, String apiUrl) async {
