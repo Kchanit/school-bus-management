cbedbdc32f8f22e0f162bc6fbc13928fae2aad97
Kchanit <chanitaphon.l@ku.th>
2023-09-10 15:05:26 +0700

    feat:add draw route

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:school_bus/app/modules/pick_address/controllers/pick_address_controller.dart';
 import 'package:school_bus/app/modules/register/controllers/register_controller.dart';
+import 'package:school_bus/app/modules/test/controllers/test_controller.dart';
 import 'package:school_bus/user_controller.dart';
 
 import 'app/routes/app_pages.dart';
@@ -11,11 +12,13 @@ void main() {
   Get.put(UserController());
   Get.put(RegisterController());
   Get.put(PickAddressController());
+  Get.put(TestController());
   runApp(
     GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      initialBinding: BindingsBuilder.put(() => TestController()),
     ),
   );
 }
