01bae8df7b291e8eb01891f72ea393a70ef752b4
Urawit <urawit.j@ku.th>
2023-09-14 23:22:34 +0700

    initialize firebase and firebaseMessaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,37 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:school_bus/app/modules/pick_address/controllers/pick_address_controller.dart';
 import 'package:school_bus/app/modules/register/controllers/register_controller.dart';
+import 'package:school_bus/firebase_options.dart';
 import 'package:school_bus/user_controller.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
   Get.put(UserController());
   Get.put(RegisterController());
   Get.put(PickAddressController());
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
+  await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+  await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
   runApp(
     GetMaterialApp(
       title: "Application",
